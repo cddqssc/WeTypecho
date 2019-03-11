77c569bc9934ba3ce149ba608eb5ea6c94f83d0d
idealclover <shadowspacex@163.com>
2019-03-12 00:06:18 +0800

    :bug: fixed bugs in category

--- we-typecho-plugin/Action.php
+++ we-typecho-plugin/Action.php
@@ -30,7 +30,7 @@ class WeTypecho_Action extends Typecho_Widget implements Widget_Interface_Do {
             $this->export('API secret error');
             }
     }
-    
+
     private function posts() {
         $sec = self::GET('apisec', 'null');
         self::checkApisec($sec);
@@ -40,7 +40,7 @@ class WeTypecho_Action extends Typecho_Widget implements Widget_Interface_Do {
         $offset   = $pageSize * ($page - 1);
         $getpage = self::GET('getpage', 0);
         $idx     = self::GET('idx',-1);
-        
+
         // 根据cid偏移获取文章
         if (isset($_GET['cid'])) {
             $cid = self::GET('cid');
@@ -105,7 +105,7 @@ class WeTypecho_Action extends Typecho_Widget implements Widget_Interface_Do {
     private function getaboutcid()
     {
         $cid = 'none';
-        $cid = Typecho_Widget::widget('Widget_Options')->plugin('WeTypecho')->aboutCid; 
+        $cid = Typecho_Widget::widget('Widget_Options')->plugin('WeTypecho')->aboutCid;
 
         $this->export($cid);
     }
@@ -129,11 +129,11 @@ class WeTypecho_Action extends Typecho_Widget implements Widget_Interface_Do {
         self::checkApisec($sec);
         $sec = self::GET('apisec', 'null');
         self::checkApisec($sec);
-        $temp = Typecho_Widget::widget('Widget_Options')->plugin('WeTypecho')->hiddenmid;                
-        $select = $this->db->select('name','slug','type','description','mid')->from('table.metas')->where('table.metas.type = ?','category');  
+        $temp = Typecho_Widget::widget('Widget_Options')->plugin('WeTypecho')->hiddenmid;
+        $select = $this->db->select('name','slug','type','description','mid')->from('table.metas')->where('table.metas.type = ?','category');
         $hiddenmids = explode(",",$temp);
         $hidden = false;
-        if(sizeof($hiddenmids)>0 && intval($hiddenmids[0])) {        
+        if(sizeof($hiddenmids)>0 && intval($hiddenmids[0])) {
         $select->where('mid in ?', $hiddenmids);
         $hidden = true;
         }
@@ -147,7 +147,7 @@ class WeTypecho_Action extends Typecho_Widget implements Widget_Interface_Do {
         }
         $this->export($cat);
     }
-    
+
     //首页参数 pageSize
     private function recentPost() {
         $sec = self::GET('apisec', 'null');
@@ -210,7 +210,7 @@ class WeTypecho_Action extends Typecho_Widget implements Widget_Interface_Do {
         {
             $openids = $this->db->fetchAll($this->db->select('openid')->from('table.wetypecholike')->where('cid = ?', $cid));
             foreach($openids as $openid) {
-                $temp = $this->db->fetchAll($this->db->select('nickname','avatarUrl')->from('table.wetypecho')->where('openid = ?', $openid));                
+                $temp = $this->db->fetchAll($this->db->select('nickname','avatarUrl')->from('table.wetypecho')->where('openid = ?', $openid));
                 if(sizeof($temp)>0) {
                     $likeinfo[] = $temp[0];
                 }
@@ -233,49 +233,49 @@ class WeTypecho_Action extends Typecho_Widget implements Widget_Interface_Do {
 
         preg_match_all("/MemTotal\s{0,}\:+\s{0,}([\d\.]+).+?MemFree\s{0,}\:+\s{0,}([\d\.]+).+?Cached\s{0,}\:+\s{0,}([\d\.]+).+?SwapTotal\s{0,}\:+\s{0,}([\d\.]+).+?SwapFree\s{0,}\:+\s{0,}([\d\.]+)/s", $str, $buf);
         preg_match_all("/Buffers\s{0,}\:+\s{0,}([\d\.]+)/s", $str, $buffers);
-    
-    
+
+
         $res['memTotal'] = round($buf[1][0]/1024, 2);
-    
+
         $res['memFree'] = round($buf[2][0]/1024, 2);
-    
+
         $res['memBuffers'] = round($buffers[1][0]/1024, 2);
         $res['memCached'] = round($buf[3][0]/1024, 2);
-    
+
         $res['memUsed'] = $res['memTotal']-$res['memFree'];
-    
+
         $res['memPercent'] = (floatval($res['memTotal'])!=0)?round($res['memUsed']/$res['memTotal']*100,2):0;
-    
-    
+
+
         $res['memRealUsed'] = $res['memTotal'] - $res['memFree'] - $res['memCached'] - $res['memBuffers']; //真实内存使用
         $res['memRealFree'] = $res['memTotal'] - $res['memRealUsed']; //真实空闲
         $res['memRealPercent'] = (floatval($res['memTotal'])!=0)?round($res['memRealUsed']/$res['memTotal']*100,2):0; //真实内存使用率
-    
+
         $res['memCachedPercent'] = (floatval($res['memCached'])!=0)?round($res['memCached']/$res['memTotal']*100,2):0; //Cached内存使用率
-    
+
         $res['swapTotal'] = round($buf[4][0]/1024, 2);
-    
+
         $res['swapFree'] = round($buf[5][0]/1024, 2);
-    
+
         $res['swapUsed'] = round($res['swapTotal']-$res['swapFree'], 2);
-    
+
         $res['swapPercent'] = (floatval($res['swapTotal'])!=0)?round($res['swapUsed']/$res['swapTotal']*100,2):0;
         if (false == ($str = @file("/proc/loadavg"))) return false;
 
         $str = explode(" ", implode("", $str));
-    
+
         $str = array_chunk($str, 4);
-    
+
         $res['loadAvg'] = implode(" ", $str[0]);
         //磁盘
         $res['DiskTotal'] = round(@disk_total_space(".")/(1024*1024*1024),3); //总
         $res['DiskFree'] = round(@disk_free_space(".")/(1024*1024*1024),3); //可用
-        
+
         //网卡
-        $strs = @file("/proc/net/dev"); 
+        $strs = @file("/proc/net/dev");
 
         for ($i = 2; $i < count($strs); $i++ )
-        {            
+        {
             preg_match_all( "/([^\s]+):[\s]{0,}(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)/", $strs[$i], $info );
             if($info[1][0] == 'eth0') {
             $res['NetOut'] = $info[10][0];
@@ -286,18 +286,18 @@ class WeTypecho_Action extends Typecho_Widget implements Widget_Interface_Do {
         if (false === ($str = @file("/proc/cpuinfo"))) return false;
 
         $str = implode("", $str);
-    
+
         @preg_match_all("/model\s+name\s{0,}\:+\s{0,}([\w\s\)\(\@.-]+)([\r\n]+)/s", $str, $model);
-    
+
         @preg_match_all("/cpu\s+MHz\s{0,}\:+\s{0,}([\d\.]+)[\r\n]+/", $str, $mhz);
-    
+
         @preg_match_all("/cache\s+size\s{0,}\:+\s{0,}([\d\.]+\s{0,}[A-Z]+[\r\n]+)/", $str, $cache);
-    
+
         @preg_match_all("/bogomips\s{0,}\:+\s{0,}([\d\.]+)[\r\n]+/", $str, $bogomips);
-    
-        if (false !== is_array($model[1]))    
+
+        if (false !== is_array($model[1]))
         {
-    
+
             $res['cpu']['num'] = sizeof($model[1]);
             if($res['cpu']['num']==1)
                 $x1 = '';
@@ -344,26 +344,26 @@ class WeTypecho_Action extends Typecho_Widget implements Widget_Interface_Do {
         $this->export("error");
     }
 
-    function http_post_data($url, $data_string) {  
-  
-        $ch = curl_init();  
-        curl_setopt($ch, CURLOPT_POST, 1);  
-        curl_setopt($ch, CURLOPT_URL, $url);  
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);  
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array(  
-            'Content-Type: application/json; charset=utf-8',  
-            'Content-Length: ' . strlen($data_string))  
-        );  
-        ob_start();  
-        curl_exec($ch);  
-        $return_content = ob_get_contents();  
+    function http_post_data($url, $data_string) {
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            'Content-Type: application/json; charset=utf-8',
+            'Content-Length: ' . strlen($data_string))
+        );
+        ob_start();
+        curl_exec($ch);
+        $return_content = ob_get_contents();
 		//echo $return_content."<br>";
-        ob_end_clean();  
-  
+        ob_end_clean();
+
         $return_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-      //  return array($return_code, $return_content);  
+      //  return array($return_code, $return_content);
 	  return  $return_content;
-    } 
+    }
     private function login()
     {
         $sec = self::GET('apisec', 'null');
@@ -452,7 +452,7 @@ class WeTypecho_Action extends Typecho_Widget implements Widget_Interface_Do {
         if($mid == 99999999) {
             $posts = $this->db->fetchAll($this->db->select('cid', 'title', 'created', 'type', 'slug','commentsNum')->from('table.contents')->where('type = ?', 'post')->where('status = ?', 'publish')->where('created < ?', time())->order('table.contents.created', Typecho_Db::SORT_DESC)->limit(10));
             foreach($posts as $post) {
-                $temp = $this->db->fetchAll($this->db->select('cid', 'title', 'created','commentsNum', 'views', 'likes')->from('table.contents')->where('cid = ?', $post['cid'])->where('status = ?', 'publish'));				
+                $temp = $this->db->fetchAll($this->db->select('cid', 'title', 'created','commentsNum', 'views', 'likes')->from('table.contents')->where('cid = ?', $post['cid'])->where('status = ?', 'publish'));
                 if(sizeof($temp)>0) {
                     $temp['0']['thumb'] = $this->db->fetchAll($this->db->select('str_value')->from('table.fields')->where('cid = ?', $post['cid']));
                     array_push($select,$temp[0]);
@@ -466,14 +466,18 @@ class WeTypecho_Action extends Typecho_Widget implements Widget_Interface_Do {
         }
         else if($mid>=0)
         {
+            $categoryListWidget = $this->widget('Widget_Metas_Category_List', 'current='.$mid);
+            $category = $categoryListWidget->filter($category);
+            $children = $categoryListWidget->getAllChildren($category['mid']);
+            $children[] = $category['mid'];
             $limit = 0;
             if($except != 'null') {
-                $posts = $this->db->fetchAll($this->db->select('cid','mid')->from('table.relationships')->where('mid = ?', $mid)->where('cid != ?', $except));
+                $posts = $this->db->fetchAll($this->db->select('cid','mid')->from('table.relationships')->where('mid IN ?', $children)->where('cid != ?', $except));
             } else {
-            $posts = $this->db->fetchAll($this->db->select('cid','mid')->from('table.relationships')->where('mid = ?', $mid));
+                $posts = $this->db->fetchAll($this->db->select('cid','mid')->from('table.relationships')->where('mid IN ?', $children));
             }
-            foreach($posts as $post) {   
-                $temp = $this->db->fetchAll($this->db->select('cid', 'title', 'created','commentsNum', 'views', 'likes')->from('table.contents')->where('cid = ?', $post['cid'])->where('status = ?', 'publish'));				
+            foreach($posts as $post) {
+                $temp = $this->db->fetchAll($this->db->select('cid', 'title', 'created','commentsNum', 'views', 'likes')->from('table.contents')->where('cid = ?', $post['cid'])->where('status = ?', 'publish'));
                 if(sizeof($temp)>0) {
                     $temp['0']['thumb'] = $this->db->fetchAll($this->db->select('str_value')->from('table.fields')->where('cid = ?', $post['cid']));
                     array_unshift($select,$temp[0]);
@@ -500,9 +504,9 @@ class WeTypecho_Action extends Typecho_Widget implements Widget_Interface_Do {
             $cids = $this->db->fetchAll($this->db->select('cid')->from('table.contents')->where('text LIKE ?', '%' . $keyword . '%'));
             if(sizeof($cids)>0){
                 foreach($cids as $cid) {
-                    $post = $this->db->fetchAll($this->db->select('cid', 'title', 'created', 'type', 'slug','commentsNum')->from('table.contents')->where('cid = ?', $cid)->where('type = ?', 'post')->where('status = ?', 'publish')->where('created < ?', time()));                
+                    $post = $this->db->fetchAll($this->db->select('cid', 'title', 'created', 'type', 'slug','commentsNum')->from('table.contents')->where('cid = ?', $cid)->where('type = ?', 'post')->where('status = ?', 'publish')->where('created < ?', time()));
                     if(sizeof($post)>0 && $post[0]!=null) {
-                        $post[0]        = $this->widget("Widget_Abstract_Contents")->push($post[0]);                  
+                        $post[0]        = $this->widget("Widget_Abstract_Contents")->push($post[0]);
                         $post[0]['tag'] = $this->db->fetchAll($this->db->select('name')->from('table.metas')->join('table.relationships', 'table.metas.mid = table.relationships.mid', Typecho_DB::LEFT_JOIN)->where('table.relationships.cid = ?', $cid)->where('table.metas.type = ?', 'tag'));
                         $post[0]['thumb'] = $this->db->fetchAll($this->db->select('str_value')->from('table.fields')->where('cid = ?', $cid))?$this->db->fetchAll($this->db->select('str_value')->from('table.fields')->where('cid = ?', $cid)):array(array("str_value"=>"https://api.isoyu.com/bing_images.php"));
                         $post[0]['views'] = $this->db->fetchAll($this->db->select('views')->from('table.contents')->where('table.contents.cid = ?', $cid));
@@ -534,9 +538,9 @@ class WeTypecho_Action extends Typecho_Widget implements Widget_Interface_Do {
         $result = array();
         if(sizeof($cids)>0){
             foreach($cids as $cid) {
-                $post = $this->db->fetchAll($this->db->select('cid', 'title', 'created', 'type', 'slug','commentsNum')->from('table.contents')->where('cid = ?', $cid)->where('status = ?', 'publish')->where('type = ?', 'post')->where('created < ?', time()));                
+                $post = $this->db->fetchAll($this->db->select('cid', 'title', 'created', 'type', 'slug','commentsNum')->from('table.contents')->where('cid = ?', $cid)->where('status = ?', 'publish')->where('type = ?', 'post')->where('created < ?', time()));
                 if(sizeof($post)>0 && $post[0]!=null) {
-                    $post[0]        = $this->widget("Widget_Abstract_Contents")->push($post[0]);                  
+                    $post[0]        = $this->widget("Widget_Abstract_Contents")->push($post[0]);
                     $post[0]['tag'] = $this->db->fetchAll($this->db->select('name')->from('table.metas')->join('table.relationships', 'table.metas.mid = table.relationships.mid', Typecho_DB::LEFT_JOIN)->where('table.relationships.cid = ?', $cid)->where('table.metas.type = ?', 'tag'));
                     $post[0]['thumb'] = $this->db->fetchAll($this->db->select('str_value')->from('table.fields')->where('cid = ?', $cid))?$this->db->fetchAll($this->db->select('str_value')->from('table.fields')->where('cid = ?', $cid)):array(array("str_value"=>"https://api.isoyu.com/bing_images.php"));
                     $post[0]['views'] = $this->db->fetchAll($this->db->select('views')->from('table.contents')->where('table.contents.cid = ?', $cid));
@@ -571,7 +575,7 @@ class WeTypecho_Action extends Typecho_Widget implements Widget_Interface_Do {
         foreach($comments as $comment) {
             if($comment['parent'] != 0) {
                 $parent = $comment['parent'];
-                $temp = $this->db->fetchAll($this->db->select('cid','coid','created', 'author', 'text', 'parent', 'authorImg')->from('table.comments')->where('cid = ?', $cid)->where('coid = ?', $parent)->where('status = ?', 'approved')->order('table.comments.created', Typecho_Db::SORT_DESC));                
+                $temp = $this->db->fetchAll($this->db->select('cid','coid','created', 'author', 'text', 'parent', 'authorImg')->from('table.comments')->where('cid = ?', $cid)->where('coid = ?', $parent)->where('status = ?', 'approved')->order('table.comments.created', Typecho_Db::SORT_DESC));
                 if(sizeof($temp)>0)
                 {
                     while($temp[0]['parent']!=0)
